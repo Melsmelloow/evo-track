556ed6f6326622bc556b7660a909ac0523b3df66
Mel Carlo <melcarlo.iguis@gmail.com>
2026-05-03 23:12:38 +0800

    chore: add axiom as observability

--- lib/axiom/server.ts
+++ lib/axiom/server.ts
@@ -1,10 +1,13 @@
-import axiomClient from '@/lib/axiom/axiom';
-import { Logger, AxiomJSTransport } from '@axiomhq/logging';
-import { createAxiomRouteHandler, nextJsFormatters } from '@axiomhq/nextjs';
+import axiomClient from "@/lib/axiom/axiom";
+import { Logger, AxiomJSTransport } from "@axiomhq/logging";
+import { createAxiomRouteHandler, nextJsFormatters } from "@axiomhq/nextjs";
 
 export const logger = new Logger({
   transports: [
-    new AxiomJSTransport({ axiom: axiomClient, dataset: process.env.NEXT_PUBLIC_AXIOM_DATASET! }),
+    new AxiomJSTransport({
+      axiom: axiomClient,
+      dataset: process.env.NEXT_AXIOM_DATASET!,
+    }),
   ],
   formatters: nextJsFormatters,
 });
